7573a226d9b126ec6a41b78418298c0375e88158
alain <alain973@gmail.com>
2022-07-18 14:58:39 +0200

    data setup
    add a user with properties like mail and password and pseudo
    and data to store recipes all link with users, the rule is a recipe
    belong to one user and a user can have many recipes.

---
+++ src/Entity/Recipe.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RecipeRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RecipeRepository::class)]
+class Recipe
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column()]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::ARRAY)]
+    private array $ingredients = [];
+
+    #[ORM\Column(type: Types::ARRAY, nullable: true)]
+    private array $utensils = [];
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $instructions = null;
+
+    #[ORM\Column(type: Types::SMALLINT, nullable: true)]
+    private ?int $preparation_time = null;
+
+    #[ORM\ManyToOne(inversedBy: 'recipes')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getIngredients(): array
+    {
+        return $this->ingredients;
+    }
+
+    public function setIngredients(array $ingredients): self
+    {
+        $this->ingredients = $ingredients;
+
+        return $this;
+    }
+
+    public function getUtensils(): array
+    {
+        return $this->utensils;
+    }
+
+    public function setUtensils(?array $utensils): self
+    {
+        $this->utensils = $utensils;
+
+        return $this;
+    }
+
+    public function getInstructions(): ?string
+    {
+        return $this->instructions;
+    }
+
+    public function setInstructions(string $instructions): self
+    {
+        $this->instructions = $instructions;
+
+        return $this;
+    }
+
+    public function getPreparationTime(): ?int
+    {
+        return $this->preparation_time;
+    }
+
+    public function setPreparationTime(?int $preparation_time): self
+    {
+        $this->preparation_time = $preparation_time;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
